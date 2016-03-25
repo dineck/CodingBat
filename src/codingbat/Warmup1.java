6a065fc415440ace8b81f46c07c36fb3a695ef85
dineck <dineckg@gmail.com>
2016-03-25 23:16:09 +0200

    de acasa 25 martie 2016

--- src/codingbat/Warmup1.java
+++ src/codingbat/Warmup1.java
@@ -2,6 +2,71 @@ package codingbat;
 
 public class Warmup1 {
 
+    public static String startOz(String str) {
+        if (str.length() == 1) {
+            if (str.charAt(0) == 'o') {
+                return "o";
+            }
+        } else if (str.length() > 0) {
+            if (str.charAt(0) == 'o' && str.charAt(1) == 'z') {
+                return "oz";
+            }
+            if (str.charAt(0) == 'o') {
+                return "o";
+            }
+        }
+        if (str.length() > 1) {
+            if (str.charAt(1) == 'z') {
+                return "z";
+            }
+        }
+        return "";
+    }
+
+    public static int intMax(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        }
+        if (b > a && b > c) {
+            return b;
+        }
+        if (c > a && c > b) {
+            return c;
+        }
+        return 0;
+    }
+
+    public static int close10(int a, int b) {
+        if (Math.abs(a - 10) < Math.abs(b - 10)) {
+            return a;
+        }
+        if (Math.abs(a - 10) > Math.abs(b - 10)) {
+            return b;
+        }
+        return 0;
+    }
+
+    public static boolean in3050(int a, int b) {
+        if ((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) {
+            return true;
+        }
+        if ((a >= 40 && a <= 50) && (b >= 40 && b <= 50)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int max1020(int a, int b) {
+        
+        if ((a >= 10 && a <= 20) && a > b ) {
+            return a;
+        }
+        if (b > a && (b >= 10 && b <= 20)) {
+            return b;
+        }
+        return 0;
+    }
+    
     public static String missingChar(String str, int n) {
         StringBuilder str1 = new StringBuilder(str);
         str = (str1.deleteCharAt(n)).toString();
@@ -76,22 +141,5 @@ public class Warmup1 {
         } else {
             return str;
         }
-    }
-
-    public static String startOz(String str) {
-        if (str.length() > 0) {
-            for (int k = 0; k < str.length(); k++) {
-                if (str.charAt(k) == 'o' && str.charAt(k + 1) == 'z' && (k+1)<str.length()) {
-                    return "oz";
-                }
-                if (str.charAt(k) == 'o' && str.charAt(k + 1) != 'z') {
-                    return "o";
-                }
-                if (str.charAt(k) == 'z') {
-                    return "z";
-                }
-            }
-        }
-        return "";
     }
 }
